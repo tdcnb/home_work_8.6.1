d9db9d64543b1643c4c219130b95f1b3b9e70cc9
Igor Evstifeev <78364050+tdcnb@users.noreply.github.com>
2022-09-21 19:40:47 +0500

    Add files via upload

---
+++ main.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Practice </title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    
+    <div class="flex-container">
+
+        <div class="header">     
+               <?php include 'logo.inc.php' ?>         
+               <?php include 'menu.inc.php' ?>	   
+        </div>       
+     
+        <div class="about_me">
+         
+          <h1>  <?php  echo $p  ?> </h1>
+
+            <div class="data">
+                <div class="myImg">
+                    <?php  echo '<img src="/img/123.jpg">'; ?>                    
+                </div>
+
+                <div class="fullname">
+                    <p> Меня зовут 
+                    <?php echo $name, ' ', $surname . '<br>'; 
+                          echo 'город', ' ', $city; ?>                                      
+                    </p> 
+           
+                    <p> Мне
+                    <?php  echo $age;   ?>          
+                    лет </p>
+                    <p> Мы познакомились с web программированием </p>
+                    <p> сайты создаются с помощью html, javascript, php </p>
+                </div>
+            </div>
+
+            <div class="knowledge">
+                                   
+                    <?php  include 'knowledge.inc.php'; ?>
+                    <?php   echo $a, ' ', $b, ' ', $c; ?> <br>
+                                       
+                    <?php
+                        $a = 10;
+                        $b = 20;
+                        $c = $a + $b;
+                        echo $c;
+                    ?>   <br>                
+                     <?php
+                        echo $d;
+                        echo $e;
+                    ?> 
+
+            </div>
+
+            <div class="article">
+                <p class="text">
+                    Рыбатекст используется дизайнерами, проектировщиками и фронтендерами, когда нужно быстро заполнить макеты или прототипы содержимым. Это тестовый контент, который не должен нести никакого смысла, лишь показать наличие самого текста или продемонстрировать типографику в деле.
+                </p>
+            </div>
+        </div>
+
+            <?php include 'footer.inc.php' ?>
+
+    </div>
+
+
+</body>
+</html>
